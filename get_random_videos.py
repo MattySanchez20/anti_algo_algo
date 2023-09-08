247bcf1913f463a4e126539a0ced9eccd796f633
Matias-Sanchez-Wilson <92598449+Matias-Sanchez-Wilson@users.noreply.github.com>
2023-09-08 15:28:17 +0100

    implementing docker file to the script

--- get_random_videos.py
+++ get_random_videos.py
@@ -11,103 +11,103 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-favourite_youtubers = ["CodyKo", "KitchenNightmares"]
+# favourite_youtubers = ["CodyKo", "KitchenNightmares"]
 
-random_youtuber = random.choice(favourite_youtubers)
+# random_youtuber = random.choice(favourite_youtubers)
 
-url = f"https://www.youtube.com/@{random_youtuber}/videos"
+# url = f"https://www.youtube.com/@{random_youtuber}/videos"
 
-options = Options()
+# options = Options()
 
-# detach means it allows chrome to run in a seperate window (set to true)
-options.add_experimental_option("detach", True)
+# # detach means it allows chrome to run in a seperate window (set to true)
+# options.add_experimental_option("detach", True)
 
-# driver uses google chrome as the web browser
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=options
-)
+# # driver uses google chrome as the web browser
+# driver = webdriver.Chrome(
+#     service=Service(ChromeDriverManager().install()), options=options
+# )
 
-# directing the driver to the desired url
-driver.get(url=url)
+# # directing the driver to the desired url
+# driver.get(url=url)
 
-driver.maximize_window()
+# driver.maximize_window()
 
-try:
-    # Scroll to the bottom of the page
-    last_height = driver.execute_script("return document.documentElement.scrollHeight")
+# try:
+#     # Scroll to the bottom of the page
+#     last_height = driver.execute_script("return document.documentElement.scrollHeight")
 
-    while True:
-        # Scroll down
-        driver.execute_script(
-            "window.scrollTo(0, document.documentElement.scrollHeight);"
-        )
+#     while True:
+#         # Scroll down
+#         driver.execute_script(
+#             "window.scrollTo(0, document.documentElement.scrollHeight);"
+#         )
 
-        # Wait for a short time to allow new content to load
-        driver.implicitly_wait(2)
+#         # Wait for a short time to allow new content to load
+#         driver.implicitly_wait(2)
 
-        # Check if the "Accept all" button is present
-        accept_button = driver.find_elements(
-            By.XPATH, "//button[@aria-label='Accept all']"
-        )
-        if accept_button:
-            accept_button[0].click()  # Click the button to accept cookies (if present)
-            break
+#         # Check if the "Accept all" button is present
+#         accept_button = driver.find_elements(
+#             By.XPATH, "//button[@aria-label='Accept all']"
+#         )
+#         if accept_button:
+#             accept_button[0].click()  # Click the button to accept cookies (if present)
+#             break
 
-        new_height = driver.execute_script(
-            "return document.documentElement.scrollHeight"
-        )
-        if new_height == last_height:
-            # If no more scrolling is possible, break the loop
-            break
-        last_height = new_height
-finally:
-    pass
+#         new_height = driver.execute_script(
+#             "return document.documentElement.scrollHeight"
+#         )
+#         if new_height == last_height:
+#             # If no more scrolling is possible, break the loop
+#             break
+#         last_height = new_height
+# finally:
+#     pass
 
-driver.refresh()
+# driver.refresh()
 
-popular_button_html = """
-//yt-formatted-string[@class='style-scope yt-chip-cloud-chip-renderer']
-"""
+# popular_button_html = """
+# //yt-formatted-string[@class='style-scope yt-chip-cloud-chip-renderer']
+# """
 
-popular_button_element = driver.find_element(By.XPATH, popular_button_html)
+# popular_button_element = driver.find_element(By.XPATH, popular_button_html)
 
-popular_button_element.click()
+# popular_button_element.click()
 
-########################################## scroll to load more videos
+# ########################################## scroll to load more videos
 
-# Replace 'your_scroll_amount' with the desired scroll amount (e.g., 2, 3, etc.)
-scroll_amount = 2
+# # Replace 'your_scroll_amount' with the desired scroll amount (e.g., 2, 3, etc.)
+# scroll_amount = 2
 
-# Calculate the number of scrolls based on the scroll amount
-num_scrolls = 10  # Total number of scrolls you want to perform
+# # Calculate the number of scrolls based on the scroll amount
+# num_scrolls = 10  # Total number of scrolls you want to perform
 
-for _ in range(num_scrolls):
-    # Scroll down
-    for _ in range(scroll_amount):
-        actions = ActionChains(driver)
-        actions.send_keys(Keys.PAGE_DOWN).perform()
+# for _ in range(num_scrolls):
+#     # Scroll down
+#     for _ in range(scroll_amount):
+#         actions = ActionChains(driver)
+#         actions.send_keys(Keys.PAGE_DOWN).perform()
 
-    # Wait briefly after scrolling
-    time.sleep(0.5)  # Adjust this value if needed
+#     # Wait briefly after scrolling
+#     time.sleep(0.5)  # Adjust this value if needed
 
-########################################
+# ########################################
 
-thumbnail_href_inside = """
-//a[@class="yt-simple-endpoint inline-block style-scope ytd-thumbnail"]
-"""
+# thumbnail_href_inside = """
+# //a[@class="yt-simple-endpoint inline-block style-scope ytd-thumbnail"]
+# """
 
-thumnail_elements = driver.find_elements(by=By.XPATH, value=thumbnail_href_inside)
+# thumnail_elements = driver.find_elements(by=By.XPATH, value=thumbnail_href_inside)
 
-random_video_urls = list()
+# random_video_urls = list()
 
-for element in thumnail_elements:
-    href_value = element.get_attribute("href")
-    if href_value is not None:
-        random_video_urls.append(href_value)
+# for element in thumnail_elements:
+#     href_value = element.get_attribute("href")
+#     if href_value is not None:
+#         random_video_urls.append(href_value)
 
-driver.quit()
+# driver.quit()
 
-random_url = random.choices(k=5, population=random_video_urls)
+# random_url = random.choices(k=5, population=random_video_urls)
 
 
 class YoutubeVideoRandomiser:
@@ -120,7 +120,7 @@ class YoutubeVideoRandomiser:
         self.favourite_youtubers = favourite_youtubers
         self.num_of_videos = num_of_videos
         
-    def _create_driver():
+    def _create_driver(self):
         options = Options()
 
         # detach means it allows chrome to run in a seperate window (set to true)
@@ -133,7 +133,7 @@ class YoutubeVideoRandomiser:
         
         return driver
         
-    def _handle_t_and_c(self, driver, xpath_to_button: str):
+    def _handle_t_and_c(self, driver, xpath_to_button: str = "//button[@aria-label='Accept all']"):
         try:
         # Scroll to the bottom of the page
             last_height = driver.execute_script("return document.documentElement.scrollHeight")
@@ -185,7 +185,7 @@ class YoutubeVideoRandomiser:
             # Wait briefly after scrolling
             time.sleep(sleep_duration)  # Adjust this value if needed
             
-    def _obtain_random_videos(self, thumbnail_href_xpath: str):
+    def _obtain_random_videos(self, driver, thumbnail_href_xpath: str, num_of_videos: int):
 
         thumnail_elements = driver.find_elements(by=By.XPATH, value=thumbnail_href_xpath)
 
@@ -198,6 +198,44 @@ class YoutubeVideoRandomiser:
 
         driver.quit()
 
-        random_url = random.choices(k=5, population=random_video_urls)
+        random_url = random.choices(k=num_of_videos, population=random_video_urls)
         
         return random_url
+    
+    def f_mark_z(self):
+        random_youtuber = random.choice(self.favourite_youtubers)
+
+        url = f"https://www.youtube.com/@{random_youtuber}/videos"
+
+        driver = self._create_driver()
+
+        # directing the driver to the desired url
+        driver.get(url=url)
+
+        driver.maximize_window()
+
+        self._handle_t_and_c(driver=driver)
+
+        popular_button_html = """
+        //yt-formatted-string[@class='style-scope yt-chip-cloud-chip-renderer']
+        """
+
+        popular_button_element = driver.find_element(By.XPATH, popular_button_html)
+
+        popular_button_element.click()
+
+        ########################################## scroll to load more videos
+
+        self._scroll_down(driver=driver)
+
+        ########################################
+
+        thumbnail_href_inside = """
+        //a[@class="yt-simple-endpoint inline-block style-scope ytd-thumbnail"]
+        """
+
+        random_vids = self._obtain_random_videos(driver=driver, thumbnail_href_xpath=thumbnail_href_inside, num_of_videos=self.num_of_videos)
+        
+        return random_vids
+
+        driver.quit()
--- origin.py
+++ origin.py
@@ -11,105 +11,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
-from email_sender import Email_Handler
+from get_random_videos import YoutubeVideoRandomiser
 
-favourite_youtubers = ["CodyKo", "KitchenNightmares"]
+yt = YoutubeVideoRandomiser(num_of_videos=1)
 
-random_youtuber = random.choice(favourite_youtubers)
+print(yt.f_mark_z())
 
-url = f"https://www.youtube.com/@{random_youtuber}/videos"
-
-options = Options()
-
-# detach means it allows chrome to run in a seperate window (set to true)
-options.add_experimental_option("detach", True)
-
-# driver uses google chrome as the web browser
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=options
-)
-
-# directing the driver to the desired url
-driver.get(url=url)
-
-driver.maximize_window()
-
-try:
-    # Scroll to the bottom of the page
-    last_height = driver.execute_script("return document.documentElement.scrollHeight")
-
-    while True:
-        # Scroll down
-        driver.execute_script(
-            "window.scrollTo(0, document.documentElement.scrollHeight);"
-        )
-
-        # Wait for a short time to allow new content to load
-        driver.implicitly_wait(2)
-
-        # Check if the "Accept all" button is present
-        accept_button = driver.find_elements(
-            By.XPATH, "//button[@aria-label='Accept all']"
-        )
-        if accept_button:
-            accept_button[0].click()  # Click the button to accept cookies (if present)
-            break
-
-        new_height = driver.execute_script(
-            "return document.documentElement.scrollHeight"
-        )
-        if new_height == last_height:
-            # If no more scrolling is possible, break the loop
-            break
-        last_height = new_height
-finally:
-    pass
-
-driver.refresh()
-
-popular_button_html = """
-//yt-formatted-string[@class='style-scope yt-chip-cloud-chip-renderer']
-"""
-
-popular_button_element = driver.find_element(By.XPATH, popular_button_html)
-
-popular_button_element.click()
-
-########################################## scroll to load more videos
-
-# Replace 'your_scroll_amount' with the desired scroll amount (e.g., 2, 3, etc.)
-scroll_amount = 2
-
-# Calculate the number of scrolls based on the scroll amount
-num_scrolls = 10  # Total number of scrolls you want to perform
-
-for _ in range(num_scrolls):
-    # Scroll down
-    for _ in range(scroll_amount):
-        actions = ActionChains(driver)
-        actions.send_keys(Keys.PAGE_DOWN).perform()
-
-    # Wait briefly after scrolling
-    time.sleep(0.5)  # Adjust this value if needed
-
-########################################
-
-thumbnail_href_inside = """
-//a[@class="yt-simple-endpoint inline-block style-scope ytd-thumbnail"]
-"""
-
-thumnail_elements = driver.find_elements(by=By.XPATH, value=thumbnail_href_inside)
-
-random_video_urls = list()
-
-for element in thumnail_elements:
-    href_value = element.get_attribute("href")
-    if href_value is not None:
-        random_video_urls.append(href_value)
-
-random_url = random.choices(k=5, population=random_video_urls)
-
-driver.quit()
 """
 print(random_url)
 #### SEND THE EMAIL WITH THE URL
