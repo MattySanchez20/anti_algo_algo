92f1af253285eccd83c4fd575bf19e48337e6702
matias-olvin <matias@olvin.com>
2023-11-17 18:06:54 +0000

    working version, revert to this is something goes wrong

--- origin.py
+++ origin.py
@@ -1,16 +1,3 @@
-import random
-import time
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
-
 from get_random_videos import YoutubeVideoRandomiser
 
 yt = YoutubeVideoRandomiser(num_of_videos=1)
